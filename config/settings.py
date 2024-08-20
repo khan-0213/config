dbb19d3495f6893c4ebaea9c628c8c477a4efba5
khan-0213 <gofforovzavqiddin981@gmail.com>
2024-08-20 16:27:58 +0500

    gunicorn ornatildi

--- config/settings.py
+++ config/settings.py
@@ -126,8 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
-STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
