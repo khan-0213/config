ad472b1f99ebb4c2be698678ceea2c305ecea931
khan-0213 <gofforovzavqiddin981@gmail.com>
2024-08-25 14:07:10 +0500

    last

--- config/settings.py
+++ config/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
@@ -146,7 +146,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 #MEDIA
 MEDIA_URL = '/media/'
-MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
+# MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 #CKEDITOR_CONFIGS
